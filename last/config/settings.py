eebec2d3bc5a21a997d8ae7cef091aec6a45d320
testindsad <dekoderabia@gmail.com>
2023-05-19 16:18:18 -0700

    tetst

--- last/config/settings.py
+++ last/config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lee-5os@c1b6t7c$eck8s6v9ze!*vps!-pg-nuy&yiu!#js*ys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False    
+DEBUG = True    
 
 
 
